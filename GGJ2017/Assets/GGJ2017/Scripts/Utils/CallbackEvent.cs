2a40bb43405642340d730632dc9570ba59ba8956
stregawolf <stregawolf@gmail.com>
2017-01-20 21:19:16 -0800

    Added WIP code for scoring

---
+++ GGJ2017/Assets/GGJ2017/Scripts/Utils/CallbackEvent.cs
@@ -0,0 +1,145 @@
+﻿// Generic event classes
+public class CallbackEvent
+{
+    public System.Action callbacks;
+
+    /// <summary>
+    /// Should be called only during initialization, make sure to call Unregister in shutdown code
+    /// </summary>
+    public void Register(System.Action callback)
+    {
+        callbacks += callback;
+    }
+
+    /// <summary>
+    /// Should be called in shutdown code
+    /// </summary>
+    public void Unregister(System.Action callback)
+    {
+        callbacks -= callback;
+    }
+
+    public void Dispatch()
+    {
+        if (callbacks != null)
+        {
+            callbacks();
+        }
+    }
+}
+
+public class CallbackEvent<T>
+{
+    public System.Action<T> callbacks;
+
+    /// <summary>
+    /// Should be called only during initialization, make sure to call Unregister in shutdown code
+    /// </summary>
+    public void Register(System.Action<T> callback)
+    {
+        callbacks += callback;
+    }
+
+    /// <summary>
+    /// Should be called in shutdown code
+    /// </summary>
+    public void Unregister(System.Action<T> callback)
+    {
+        callbacks -= callback;
+    }
+
+    public void Dispatch(T data)
+    {
+        if(callbacks != null)
+        {
+            callbacks(data);
+        }
+    }
+}
+
+public class CallbackEvent<T1, T2>
+{
+    public System.Action<T1,T2> callbacks;
+
+    /// <summary>
+    /// Should be called only during initialization, make sure to call Unregister in shutdown code
+    /// </summary>
+    public void Register(System.Action<T1, T2> callback)
+    {
+        callbacks += callback;
+    }
+
+    /// <summary>
+    /// Should be called in shutdown code
+    /// </summary>
+    public void Unregister(System.Action<T1, T2> callback)
+    {
+        callbacks -= callback;
+    }
+
+    public void Dispatch(T1 data1, T2 data2)
+    {
+        if (callbacks != null)
+        {
+            callbacks(data1, data2);
+        }
+    }
+}
+
+public class CallbackEvent<T1, T2, T3>
+{
+    public System.Action<T1, T2, T3> callbacks;
+
+    /// <summary>
+    /// Should be called only during initialization, make sure to call Unregister in shutdown code
+    /// </summary>
+    public void Register(System.Action<T1, T2, T3> callback)
+    {
+        callbacks += callback;
+    }
+
+    /// <summary>
+    /// Should be called in shutdown code
+    /// </summary>
+    public void Unregister(System.Action<T1, T2, T3> callback)
+    {
+        callbacks -= callback;
+    }
+
+    public void Dispatch(T1 data1, T2 data2, T3 data3)
+    {
+        if (callbacks != null)
+        {
+            callbacks(data1, data2, data3);
+        }
+    }
+}
+
+public class CallbackEvent<T1, T2, T3, T4>
+{
+    public System.Action<T1, T2, T3, T4> callbacks;
+
+    /// <summary>
+    /// Should be called only during initialization, make sure to call Unregister in shutdown code
+    /// </summary>
+    public void Register(System.Action<T1, T2, T3, T4> callback)
+    {
+        callbacks += callback;
+    }
+
+    /// <summary>
+    /// Should be called in shutdown code
+    /// </summary>
+    public void Unregister(System.Action<T1, T2, T3, T4> callback)
+    {
+        callbacks -= callback;
+    }
+
+    public void Dispatch(T1 data1, T2 data2, T3 data3, T4 data4)
+    {
+        if (callbacks != null)
+        {
+            callbacks(data1, data2, data3, data4);
+        }
+    }
+}
---
+++ GGJ2017/Assets/GGJ2017/Scripts/Utils/EventManager.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class EventManager {
+    public static readonly CallbackEvent Player1Scored = new CallbackEvent();
+    public static readonly CallbackEvent Player2Scored = new CallbackEvent();
+}
