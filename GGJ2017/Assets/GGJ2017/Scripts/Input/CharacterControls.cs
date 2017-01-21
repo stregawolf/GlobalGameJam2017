188b9cb53ce7c992a4fc9ede1e84edf625b0d69c
stregawolf <stregawolf@gmail.com>
2017-01-20 20:55:15 -0800

    WackyWavyPrototype
    Added prototype scene and basic mechanics

---
+++ GGJ2017/Assets/GGJ2017/Scripts/Input/CharacterControls.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControls : MonoBehaviour {
+    public Rigidbody m_head;
+    public Rigidbody m_leftArm;
+    public Rigidbody m_rightArm;
+    public Rigidbody m_forcePoint;
+
+    public Color m_playerColor = Color.white;
+
+    public float m_passiveForceHead = 50.0f;
+    public float m_passiveForceArm = 25.0f;
+
+    public float m_force = 50.0f;
+
+    public KeyCode m_upkey = KeyCode.UpArrow;
+    public KeyCode m_downKey = KeyCode.DownArrow;
+    public KeyCode m_leftKey = KeyCode.LeftArrow;
+    public KeyCode m_rightKey = KeyCode.RightArrow;
+
+    protected void Start()
+    {
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+        for(int i = 0; i < renderers.Length; ++i)
+        {
+            renderers[i].material.color = m_playerColor;
+        }
+    }
+    
+    protected void Update()
+    {
+        m_head.AddForce(Vector3.up * m_passiveForceHead);
+        m_leftArm.AddForce((Vector3.left + Vector3.up).normalized * m_passiveForceArm);
+        m_rightArm.AddForce((Vector3.right+Vector3.up).normalized * m_passiveForceArm);
+
+        Vector3 dir = Vector3.zero;
+        if(Input.GetKey(m_upkey))
+        {
+            dir += Vector3.up;
+        }
+        if (Input.GetKey(m_leftKey))
+        {
+            dir += Vector3.left;
+        }
+        if (Input.GetKey(m_rightKey))
+        {
+            dir += Vector3.right;
+        }
+        if(Input.GetKey(m_downKey))
+        {
+            dir += Vector3.down;
+        }
+
+        dir.Normalize();
+        m_forcePoint.AddForce(dir * m_force);
+    }
+}
---
+++ GGJ2017/Assets/GGJ2017/Scripts/Objects/Actors/Ball.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float m_initialImpulse = 10.0f;
+    public float m_contactImpulse = 1.0f;
+
+    public Rigidbody m_rigidbody;
+
+    protected Vector3 m_originalPosition;
+    protected Quaternion m_originalRotation;
+
+	// Use this for initialization
+	void Start () {
+
+        m_originalPosition = transform.position;
+        m_originalRotation = transform.rotation;
+        Reset();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Space))
+        {
+            Reset();
+        }
+	}
+
+    public void Reset()
+    {
+        transform.position = m_originalPosition;
+        transform.rotation = m_originalRotation;
+        m_rigidbody.velocity = Vector3.zero;
+        m_rigidbody.angularVelocity = Vector3.zero;
+        m_rigidbody.AddForce(transform.forward * m_initialImpulse, ForceMode.Impulse);
+    }
+
+    
+    public void OnCollisionExit(Collision c)
+    {
+        if(c.collider.gameObject.layer == LayerMask.NameToLayer("Character"))
+        {
+            m_rigidbody.AddForce(Vector3.up * m_contactImpulse, ForceMode.Impulse);
+        }
+    }
+    
+}
