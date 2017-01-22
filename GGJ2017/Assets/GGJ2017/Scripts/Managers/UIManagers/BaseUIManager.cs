1bc8d3db55c90f35fd2c927109cca3402e49a845
stregawolf <stregawolf@gmail.com>
2017-01-21 18:26:58 -0800

    Restructured UI scene system to not use scene ndexes, added stub scene for title screen

---
+++ GGJ2017/Assets/GGJ2017/Scripts/Managers/UIManagers/BaseUIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseUIManager : MonoBehaviour {
+
+    public void DoSpinTransitionIn(GameObject obj, float displayTime, float transitionTime, Vector3 toScale, float spinMultiplier = 2, bool autoHide = true)
+    {
+        LeanTween.cancel(obj);
+        obj.SetActive(true);
+
+        obj.transform.localScale = Vector3.zero;
+        obj.transform.rotation = Quaternion.identity;
+        LeanTween.rotateZ(obj, 360 * spinMultiplier, transitionTime * 0.9f);
+        LeanTween.scale(obj, toScale, transitionTime).setEase(LeanTweenType.easeSpring).setIgnoreTimeScale(true)
+            .setOnComplete(() =>
+            {
+                if (autoHide)
+                {
+                    DoSpinTransitionOut(obj, transitionTime, spinMultiplier, displayTime);
+                }
+            });
+    }
+
+    public void DoSpinTransitionOut(GameObject obj, float transitionTime, float spinMultiplier = 2, float delay = 0.0f)
+    {
+        LeanTween.rotateZ(obj, 360 * spinMultiplier, transitionTime).setDelay(delay);
+        LeanTween.scale(obj, Vector3.zero, transitionTime).setDelay(delay).setEase(LeanTweenType.easeSpring).setOnComplete(() =>
+        {
+            obj.SetActive(false);
+        });
+    }
+}
--- GGJ2017/Assets/GGJ2017/Scripts/Managers/UIManagers/VolleyBallUIManager.cs
+++ GGJ2017/Assets/GGJ2017/Scripts/Managers/UIManagers/VolleyBallUIManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIManager : MonoBehaviour {
+public class VolleyBallUIManager : BaseUIManager {
     public Text m_centerText;
 
     public void Awake()
@@ -42,30 +42,4 @@ public class UIManager : MonoBehaviour {
         DoSpinTransitionIn(m_centerText.gameObject, displayTime, transitionTime, toScale);
     }
 
-    public void DoSpinTransitionIn(GameObject obj, float displayTime, float transitionTime, Vector3 toScale, float spinMultiplier = 2, bool autoHide = true)
-    {
-        LeanTween.cancel(obj);
-        obj.SetActive(true);
-
-        obj.transform.localScale = Vector3.zero;
-        obj.transform.rotation = Quaternion.identity;
-        LeanTween.rotateZ(obj, 360 * spinMultiplier, transitionTime * 0.9f);
-        LeanTween.scale(obj, toScale, transitionTime).setEase(LeanTweenType.easeSpring).setIgnoreTimeScale(true)
-            .setOnComplete(() =>
-            {
-                if (autoHide)
-                {
-                    DoSpinTransitionOut(obj, transitionTime, spinMultiplier, displayTime);
-                }
-            });
-    }
-
-    public void DoSpinTransitionOut(GameObject obj, float transitionTime, float spinMultiplier = 2, float delay = 0.0f)
-    {
-        LeanTween.rotateZ(obj, 360 * spinMultiplier, transitionTime).setDelay(delay);
-        LeanTween.scale(obj, Vector3.zero, transitionTime).setDelay(delay).setEase(LeanTweenType.easeSpring).setOnComplete(() =>
-        {
-            obj.SetActive(false);
-        });
-    }
 }
