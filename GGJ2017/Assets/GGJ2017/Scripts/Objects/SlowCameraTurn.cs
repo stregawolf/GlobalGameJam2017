e831558a89f57ac2626ea9705b980931e9c12b4a
Ted <gamedev@edwarddinola.com>
2017-01-21 21:31:20 -0800

    Moving skybox

---
+++ GGJ2017/Assets/GGJ2017/Scripts/Objects/SlowCameraTurn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowCameraTurn : MonoBehaviour {
+
+	public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Euler(0, Time.time * speed, 0);
+	}
+}
