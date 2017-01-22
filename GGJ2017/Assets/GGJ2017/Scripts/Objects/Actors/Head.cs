3b7bae33e8f2eee9c38d6e90af876d02c23e1f07
stregawolf <stregawolf@gmail.com>
2017-01-21 21:24:19 -0800

    Faces display first pass

---
+++ GGJ2017/Assets/GGJ2017/Scripts/Objects/Actors/Head.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Head : MonoBehaviour {
+    public Transform m_pivot;
+    public SpriteRenderer m_face;
+
+    public void LateUpdate()
+    {
+        Vector3 dir = m_pivot.transform.position - Camera.main.transform.position;
+        dir.Normalize();
+        m_pivot.rotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(dir, m_pivot.up), m_pivot.up);
+    }
+}
--- GGJ2017/Assets/GGJ2017/Scripts/Objects/Actors/Player.cs
+++ GGJ2017/Assets/GGJ2017/Scripts/Objects/Actors/Player.cs
@@ -5,11 +5,32 @@ using UnityEngine;
 public class Player : BaseActor
 {
     public Game.Team m_team;
+    public CharacterData m_data;
+
+    public Head m_head;
+
+    public PlayerBuilder m_builder;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        if(m_builder == null)
+        {
+            m_builder = GetComponent<PlayerBuilder>();
+        }
+    }
 
     public void Init(Game.Team team)
     {
         m_team = team;
+        m_team.m_displayName = m_data.m_displayName;
+        m_team.m_color = m_data.m_color;
+
+        m_builder.InitFromData(m_data);
+        m_builder.BuildPlayer();
+        m_head = GetComponentInChildren<Head>();
+
         DiscoverRenderers();
-        SetColor(m_team.m_color);
+        SetColor(m_data.m_color);
     }
 }
