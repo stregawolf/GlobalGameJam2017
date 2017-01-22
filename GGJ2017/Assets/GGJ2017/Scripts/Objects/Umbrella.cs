621c249778dce5c92c0a87ec8d3f8d093f23d1b1
Ted <gamedev@edwarddinola.com>
2017-01-22 04:47:16 -0800

    Umbrellas sway when hit

---
+++ GGJ2017/Assets/GGJ2017/Scripts/Objects/Umbrella.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Umbrella : MonoBehaviour {
+
+	Vector3 targetUp;
+	Rigidbody rb;
+
+	public float strength = 30.0f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.centerOfMass = Vector3.zero;
+		targetUp = transform.up;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var rot = Quaternion.FromToRotation(transform.up, Vector3.forward);
+		rb.AddTorque(new Vector3(rot.x, rot.y, rot.z)*strength);
+	}
+}
