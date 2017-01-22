3e2eea052929339d6963e1f4ad557446463905f4
Ted <gamedev@edwarddinola.com>
2017-01-21 19:10:05 -0800

    Nothing but net

---
+++ GGJ2017/Assets/GGJ2017/Scripts/Objects/Environment/Net.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Net : MonoBehaviour {
+
+	public float springStrength = 1000.0f;
+	public float minDist = 0.2f;
+	public float maxDist = 1.0f;
+	public float damper = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+		SpringJoint[] joints = GetComponentsInChildren<SpringJoint>();
+		foreach (SpringJoint joint in joints) {
+			joint.spring = springStrength;
+			joint.minDistance = minDist;
+			joint.maxDistance = maxDist;
+			joint.damper = damper;
+
+			Rigidbody rbody = joint.GetComponent<Rigidbody>();
+			rbody.freezeRotation = true;
+		}
+	}
+}
