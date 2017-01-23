8e04cc2ef69f69b51162b5574c3a4e8e7a9f053a
Ted <gamedev@edwarddinola.com>
2017-01-22 16:44:38 -0800

    Fixed music looping forever

---
+++ GGJ2017/Assets/GGJ2017/Scripts/Utils/MusicPlayerPersistence.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayerPersistence : MonoBehaviour {
+
+    public static GameObject instance;
+    static string myObjName;
+
+	// Use this for initialization
+	void Start()
+	{
+        if (instance != null)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this.gameObject;
+            instance.GetComponent<AudioSource>().Play();
+            GameObject.DontDestroyOnLoad(this.gameObject);
+        }
+	}
+}
